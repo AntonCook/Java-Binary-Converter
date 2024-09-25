f43bee01d1f061598a81b1ce12f621e5b1158a9c
moomoojuice2606 <anton.cook2053@gmail.com>
2024-09-24 19:04:12 -0500

    Adding Files for Multi Device Development

---
+++ BinConGui/src/main/java/trustworthy/bincongui/BinConGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package trustworthy.bincongui;
+
+/**
+ *
+ * @author dave
+ */
+public class BinConGui {
+
+    public static void main(String[] args) {
+        
+    }
+}
